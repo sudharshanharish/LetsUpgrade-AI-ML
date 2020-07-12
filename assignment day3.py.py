71560f71d5a1103cd0db418e16d59f3ef39c3eab
sudharshan v <sudharshanharish.vr@gmail.com>
2020-07-12 14:06:47 +0530

    Add files via upload

---
+++ assignment day3.py.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#wrute a program to subtract two complex numbers
+complexno=4+7j
+complexno1=1+15j
+print('Subtraction of two complex numbers=',complexno-complexno1)
+    
+
+
+# In[2]:
+
+
+#write a program to find the fourth root of a number
+import math
+
+math.pow(3,4)
+
+
+# In[3]:
+
+
+#swap two numbers with help of a temporary variable
+
+num1=input('Enter first number')
+num2=input('Enter second number')
+
+print("value is before",num1)
+print("value is before",num2)
+
+temp=num1
+num1=num2
+num2=temp
+
+print("value of num1 after swapping ",num1)
+print("value of num2 after swapping",num2)
+
+
+# In[4]:
+
+
+#without using temporary variable
+
+num1=45
+num2=63
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+
+print("num1 is",num1, "num2 is",num2)
+
+
+# In[5]:
+
+
+#celsius to farenheit
+
+celsius=int(input("Enter the temperature in celcius:"))
+f=(celsius*1.8)+32
+print("Temperature in farenheit is:",f)
+
+
+# In[8]:
+
+
+#data types
+# Python program to  
+# demonstrate data types
+  
+a = 5
+print("Type of a: ", type(a)) 
+  
+b = 5.0
+print("\nType of b: ", type(b)) 
+  
+c = 2 + 4j
+print("\nType of c: ", type(c)) 
+
+d='alpha'
+print("\ntype of d:",type(d))
+
+e='true'
+print(type(e))
+
+
+
+
+# In[9]:
+
+
+#steps
+#Go to https://github.com/join .
+#Type a user name, your email address, and a password.
+#Choose Sign up for GitHub, and then follow the instructions.
+#create a repository and upload the assignments
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
